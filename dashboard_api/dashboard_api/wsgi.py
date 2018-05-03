0f07f0275ccc5cfc3f79b06e17d710e57aab0863
Paul Haesler <paul.haesler@nicta.com.au>
2018-05-03 14:14:52 +1000

    Shane's theory of wsgi relativity.

--- dashboard_api/dashboard_api/wsgi.py
+++ dashboard_api/dashboard_api/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import sys
-sys.path.append('/home/phaesler/src/dashboard/dashboard_api')
-
 import os
+
+base_path = '{0}/../..'.format(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append('{0}/dashboard_api'.format(base_path))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard_api.settings")
 
 from django.core.wsgi import get_wsgi_application
