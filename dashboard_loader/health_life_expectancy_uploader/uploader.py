afc7f23039f153ba377baf41b7b5eabadc8a7207
Paul Haesler <paul.haesler@nicta.com.au>
2018-06-01 13:39:31 +1000

    Men/Women -> Males/Females life expectancy.

--- dashboard_loader/health_life_expectancy_uploader/uploader.py
+++ dashboard_loader/health_life_expectancy_uploader/uploader.py
@@ -101,25 +101,25 @@ def upload_file(uploader, fh, actual_freq_display=None, verbosity=0):
         set_statistic_data('life_expectancy-health-hero', 'life_expectancy-health-hero',
                         'men_life_exp',
                         latest_aust.males,
-                        label="Men (%s)" % latest_aust.year_display(),
+                        label="Males (%s)" % latest_aust.year_display(),
                         traffic_light_code=male_tlc,
                         trend=male_trend)
         set_statistic_data('life_expectancy-health-hero', 'life_expectancy-health-hero',
                         'women_life_exp',
                         latest_aust.females,
-                        label="Women (%s)" % latest_aust.year_display(),
+                        label="Females (%s)" % latest_aust.year_display(),
                         traffic_light_code=female_tlc,
                         trend=female_trend)
         set_statistic_data('health_life_expectancy', 'health_life_expectancy',
                         'men_life_exp',
                         latest_aust.males,
-                        label="Men (%s)" % latest_aust.year_display(),
+                        label="Males (%s)" % latest_aust.year_display(),
                         traffic_light_code=male_tlc,
                         trend=male_trend)
         set_statistic_data('health_life_expectancy', 'health_life_expectancy',
                         'women_life_exp',
                         latest_aust.females,
-                        label="Women (%s)" % latest_aust.year_display(),
+                        label="Females (%s)" % latest_aust.year_display(),
                         traffic_light_code=female_tlc,
                         trend=female_trend)
         messages.extend(
